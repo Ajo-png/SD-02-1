ac8c504c5ec810b151466fae64669c3da2d6804c
Hageo Balam <hageo.balam21@gmail.com>
2025-12-15 15:29:14 -0600

    Se agrega soluciones ejercicios 1, 2, 3, 4 y 6

--- task1.js
+++ task1.js
@@ -1,3 +1,4 @@
 // Refer to Task 1 in your Instructions to complete this task
-const prompt = require("prompt-sync")();
-const name = prompt("Ingresa tu nombre");
+for(let i = 1; i <= 105; i++){
+    console.log(i);
+}
--- task6.js
+++ task6.js
@@ -1,5 +1,24 @@
 // Refer to Task 6 in your Instructions to complete this task
+const res = [];
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Six!");
-  };
+for(let i = 1; i <= 105; i++){
+  if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0){
+    res.push("FizzBuzzWoof");
+  } else if(i % 3 == 0 && i % 7 == 0){
+    res.push("FizzWoof");
+  } else if (i % 5 == 0 && i % 7 == 0){
+    res.push("BuzzWoof");
+  } else if (i % 3 == 0 && i % 5 == 0){
+    res.push("FizzBuzz");
+  } else if (i % 3 == 0){
+    res.push("Fizz");
+  } else if (i % 5 == 0){
+    res.push("Buzz");
+  } else if (i % 7 == 0){
+    res.push("Woof");
+  } else {
+    res.push(i);
+  }
+}
+
+console.log(res);
